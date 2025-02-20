a7574b266982a694a84d0d089c3d7f5c3a3b4afa
odkeyo <100173452+odkeyo@users.noreply.github.com>
2025-02-20 13:50:17 -0600

    Update ApiController.cs

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -7,6 +7,6 @@ public class DemoTestController  : ControllerBase
     [HttpGet]
     public IActionResult Get()
     {
-        return Ok(new { message = "Wake up, Neo... The API has you." });
+        return Ok(new { message = "Hi from the Api." });
     }
 }
